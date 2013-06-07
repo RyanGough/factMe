acf6695db6bbde0d06a0fdf51ab0c6943bc9dc60
Ryan Gough <ryan.gough@gmail.com>
2013-06-07 23:39:07 +0100

    parse wiki page into sentances

--- factMe.js
+++ factMe.js
@@ -1,9 +1,19 @@
 var jscrape = require('jscrape');
 
-jscrape ( 'http://en.wikipedia.org/wiki/London', function ( error, $, response, body ) {
+var wikiPageURL = 'http://en.wikipedia.org/wiki/' + process.argv[2];
+
+jscrape ( wikiPageURL, function ( error, $, response, body ) {
 	if ( !error && $ ) {
-		var bodyContent = $('#bodyContent');
-		console.log(bodyContent.text());
+		var bodyContent = $('#bodyContent').text();
+		var paras = $('#bodyContent p').each(function(index){
+			var para = $(this).text();
+			var sentances = para.split('.');
+			sentances.forEach(function(sentance){
+				if (sentance.length) {
+					console.log(sentance.trim() + ".");
+				}
+			});
+		});
 	} else {
 		console.log("failed to get url");
 	}
